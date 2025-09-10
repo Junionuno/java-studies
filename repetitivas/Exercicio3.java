7c26b52ecb2e56048614f3793af4351f6e0fe918
Junio Geraldo <junio.geraldo@outlook.com>
2025-09-10 14:50:11 -0300

    Exercícios realizados

---
+++ repetitivas/Exercicio3.java
@@ -0,0 +1,32 @@
+package repetitivas;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int escolha = 0;
+		int alc = 0, gas = 0, die = 0;
+		
+		
+		while(escolha != 4) {
+			escolha = sc.nextInt();
+			
+			if (escolha > 4 || escolha < 1) {
+				System.out.println("Código inválido, digite novamente: ");
+			}else if(escolha == 1) {
+				alc += 1;
+			}else if(escolha == 2) {
+				gas += 1;
+			}else if(escolha == 3) {
+				die += 1;
+			}
+		}
+		
+		System.out.println("MUITO OBRIGADO!");
+		System.out.printf("Álcool: %d %nGasolina: %d %nDiesel: %d", alc, gas, die);
+		
+		sc.close();
+	}
+}
