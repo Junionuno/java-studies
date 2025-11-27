04204ce7c5dd0ed765eb6574344f3f14f9187853
Junionuno <junio.geraldo@outlook.com>
2025-11-27 17:32:33 -0300

    Exemplo 2 de membros estáticos

--- POO/main3.java
+++ POO/main3.java
@@ -1,28 +1,33 @@
 package POO;
-import util.Calculator;
-
 import java.util.Locale;
 import java.util.Scanner;
 
 public class main3 {
+    public static final double PI = 3.14159;
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Calculator calc = new Calculator();
-
         System.out.print("Enter the radius: ");
         double radius = sc.nextDouble();
 
-        double c = calc.circumference(radius);
+        double c = circumference(radius);
 
-        double v = calc.volume(radius);
+        double v = volume(radius);
 
         System.out.printf("Circumference: %.2f%n", c);
         System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI value: %.2f%n", calc.PI);
+        System.out.printf("PI value: %.2f%n", PI);
 
         sc.close();
+    }
+
+    public static double circumference(double radius){
+        return 2.0 * PI * radius;
+    }
+
+    public static double volume(double radius){
+        return 4.0 * PI * radius * radius * radius / 3.0;
     }
 }
