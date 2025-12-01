4d75d9d4e4f15135a2a2ac3da52013820f42a68c
Junionuno <junio.geraldo@outlook.com>
2025-12-01 19:39:05 -0300

    ´@Últimos exercícios do capítulo 9

---
+++ entities/Cilindro.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Cilindro {
+    public double raio;
+    public double altura;
+
+    public double volume(){
+        return 3.14159*(Math.pow(raio, 2))*altura;
+    }
+
+    public double area(){
+        return 2 * 3.14159 * raio * (raio + altura);
+    }
+
+    public void updateValue(double r, double a){
+        this.raio = r;
+        this.altura = a;
+    }
+}
