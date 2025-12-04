7d4b56f925a0a8af474a0aca4761e0869761f6a0
Junionuno <junio.geraldo@outlook.com>
2025-12-04 18:15:25 -0300

    ExerÂcÂcício de fixação do capítulo sobre construtores, sobrecarga e encapsulamento

---
+++ POO/exercicio5.java
@@ -0,0 +1,54 @@
+package POO;
+
+import entities.BankAccount;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio5 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        BankAccount bank;
+
+        System.out.print("Enter account number: ");
+        int numberAc = sc.nextInt();
+        System.out.print("Enter account holder: ");
+        String name = sc.next();
+        System.out.print("Is there a initial deposit (y/n)? ");
+        char initialD = sc.next().charAt(0);
+
+        if (initialD == 'y') {
+            System.out.print("Enter initial deposit value: ");
+            double initialDeposit = sc.nextDouble();
+            bank = new BankAccount(numberAc, name, initialDeposit);
+        } else {
+            bank = new BankAccount(numberAc, name);
+        }
+        System.out.println();
+
+        System.out.println("Account data: ");
+        System.out.println(bank);
+        System.out.println();
+
+        System.out.print("Enter a deposit value: ");
+        double value = sc.nextDouble();
+        bank.deposit(value);
+
+        System.out.println();
+        System.out.println("Updated data: ");
+        System.out.println(bank);
+
+        System.out.println();
+        System.out.print("Enter a withdraw value: ");
+        value = sc.nextDouble();
+        bank.withdraw(value);
+        System.out.println();
+
+        System.out.print("Updated data: ");
+        System.out.println(bank);
+
+        sc.close();
+    }
+}
---
+++ entities/BankAccount.java
@@ -0,0 +1,44 @@
+package entities;
+
+public class BankAccount {
+    private int numberAc;
+    private String name;
+    private double value;
+
+    public BankAccount(int numberAc, String name, double initialDeposit) {
+        this.name = name;
+        this.numberAc = numberAc;
+        deposit(initialDeposit);
+    }
+
+    public BankAccount(int numberAc, String name){
+        this.numberAc = numberAc;
+        this.name = name;
+    }
+
+    public int getNumberAc(){
+        return numberAc;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getValue(){
+        return value;
+    }
+
+    public void deposit(double value){
+        this.value += value;
+    }
+
+    public void withdraw(double value){
+        this.value = this.value - value - 5;
+    }
+
+    public String toString(){
+        return "Account " + numberAc +
+                ", Holder: " + name +
+                ", Balance: " + String.format("%.2f", value);
+    }
+}
