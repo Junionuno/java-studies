0da70a202f5f70eac6662b03a189dc717c0cbeb4
Junionuno <junio.geraldo@outlook.com>
2025-12-17 19:22:22 -0300

    Teste proposto pelo Chat GPT parecido com o desafio feito anteriormente

---
+++ entitiesVetores/BikeRent.java
@@ -0,0 +1,32 @@
+package entitiesVetores;
+
+public class BikeRent {
+    private String name;
+    private int phone;
+
+    public BikeRent(String name, int phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", " + phone;
+    }
+}
---
+++ vetores/desafioGPT.java
@@ -0,0 +1,40 @@
+package vetores;
+
+import entitiesVetores.BikeRent;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class desafioGPT {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        BikeRent[] bike = new BikeRent[8];
+
+        System.out.print("Quantas bicicletas serão alugadas? ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i<=n;i++){
+            System.out.println("Aluguel #" + i );
+            System.out.print("Nome: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Telefone: ");
+            int phone = sc.nextInt();
+            System.out.print("Bicicleta: ");
+            int bikeNumber = sc.nextInt();
+
+            bike[bikeNumber] = new BikeRent(name, phone);
+        }
+
+        System.out.println("Bicicletas alugadas: ");
+        for (int i=0;i<=7;i++){
+            if(bike[i] != null){
+                System.out.println(i + ": " + bike[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
