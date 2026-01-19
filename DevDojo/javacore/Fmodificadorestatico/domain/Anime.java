51f841232e6ebfebb31c40ba4d41c5462c592623
Junionuno <junio.geraldo@outlook.com>
2026-01-19 18:01:12 -0300

    Exemplo de bloco de inicialização estático

---
+++ DevDojo/javacore/Fmodificadorestatico/domain/Anime.java
@@ -0,0 +1,38 @@
+package DevDojo.javacore.Fmodificadorestatico.domain;
+
+public class Anime {
+    private String nome;
+    private static int[] episodios;
+
+    static {
+        System.out.println("Dentro do bloco de inicialização 1");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i+1;
+        }
+    }
+    static {
+        System.out.println("Dentro do bloco de inicialização 2");
+    }
+    static {
+        System.out.println("Dentro do bloco de inicialização 3");
+    }
+
+    public Anime(String nome){
+        this.nome = nome;
+    }
+
+    public Anime(){
+        for(int episodios : Anime.episodios){
+            System.out.println(episodios + " ");
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+}
