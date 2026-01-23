073172a26f836daf67b0e1025f2c8e6d45bfae34
Junionuno <junio.geraldo@outlook.com>
2026-01-23 20:15:19 -0300

    AlExemplo de sequência de inicialização com herança

---
+++ DevDojo/javacore/Hheranca/test/HerancaTest02.java
@@ -0,0 +1,9 @@
+package DevDojo.javacore.Hheranca.test;
+
+import DevDojo.javacore.Hheranca.dominio.Funcionario;
+
+public class HerancaTest02 {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("jiraya");
+    }
+}
