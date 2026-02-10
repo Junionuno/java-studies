80235ad9e05cb3d9b6b74d756a4b5232b161a701
Junionuno <junio.geraldo@outlook.com>
2026-02-10 20:15:26 -0300

    Exemplo sobre StackOverflow

---
+++ DevDojo/javacore/Oexceptions/test/StackOverflowTest01.java
@@ -0,0 +1,11 @@
+package DevDojo.javacore.Oexceptions.test;
+
+public class StackOverflowTest01 {
+    public static void main(String[] args) {
+        recursividade();
+    }
+
+    public static void recursividade(){
+        recursividade();
+    }
+}
