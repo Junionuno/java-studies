c55ca10980ecac1cd21cdd097e29555ed9662c11
Junionuno <junio.geraldo@outlook.com>
2026-02-11 20:36:09 -0300

    Exemplos de exceções  checked/unchecked lançadas

---
+++ DevDojo/javacore/Oexceptions/runtime/test/RuntimeExceptionTest02.java
@@ -0,0 +1,21 @@
+package DevDojo.javacore.Oexceptions.runtime.test;
+
+public class RuntimeExceptionTest02 {
+    public static void main(String[] args) {
+        divisao(1,0);
+    }
+
+    /**
+     *
+     * @param a
+     * @param b não pode ser 0.
+     * @return
+     * @throws IllegalArgumentException caso b seja 0.
+     */
+    private static int divisao(int a, int b){
+        if(b == 0){
+            throw new IllegalArgumentException("Argumento ilegal, não pode ser 0");
+        }
+        return a/b;
+    }
+}
