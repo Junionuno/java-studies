d9bf01bf410a7d5a7e2f361992b9bd7c0b963b88
Junionuno <junio.geraldo@outlook.com>
2026-02-12 20:37:52 -0300

    Exemplos de Multi catch em linha

--- DevDojo/javacore/Oexceptions/runtime/test/RuntimeExceptionTest04.java
+++ DevDojo/javacore/Oexceptions/runtime/test/RuntimeExceptionTest04.java
@@ -1,6 +1,7 @@
 package DevDojo.javacore.Oexceptions.runtime.test;
 
 import java.awt.image.AbstractMultiResolutionImage;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -8,25 +9,16 @@ import java.sql.SQLException;
 public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
         try {
-            throw new IndexOutOfBoundsException();
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
-        }catch (IndexOutOfBoundsException e){
-            System.out.println("Dentro do IndexOutOfBoundsException");
-        }catch (IllegalArgumentException e){
-            System.out.println("Dentro do IllegalArgumentException");
-        }catch (ArithmeticException e){
-            System.out.println("Dentro do ArithmeticException");
-        }
-        catch (RuntimeException e){
+            throw new ArrayIndexOutOfBoundsException();
+        }catch (IndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e){
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException");
+        }catch (RuntimeException e){
             System.out.println("Dentro do RuntimeException");
         }
 
         try{
             talvezLanceException();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }catch (IOException e){
+        }catch (SQLException | IOException e){
             e.printStackTrace();
         }
 
