a3f9f8e2ee172a2ae4155a96809c42b75f8ed29f
Junionuno <junio.geraldo@outlook.com>
2026-02-19 19:54:05 -0300

    Exemplos utilizando as classes StringBuilder e StringBuffer

---
+++ DevDojo/javacore/Qstring/test/StringBuilderTest01.java
@@ -0,0 +1,20 @@
+package DevDojo.javacore.Qstring.test;
+
+public class StringBuilderTest01 {
+    public static void main(String[] args) {
+        String nome = "William Suane";
+        nome.concat(" DevDojo");
+        nome.substring(0,3);
+        System.out.println(nome);
+
+        StringBuilder sb = new StringBuilder("William Suane");
+        sb.append(" DevDojo").append(" Academy");
+        System.out.println(sb);
+        sb.reverse();
+        System.out.println(sb);
+        sb.reverse();
+        System.out.println(sb);
+        sb.delete(0,3);
+        System.out.println(sb);
+    }
+}
