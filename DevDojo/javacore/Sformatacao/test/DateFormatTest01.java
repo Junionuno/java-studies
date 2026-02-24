810f2890b503132d2d6326d935f9651a7d5fc72f
Junionuno <junio.geraldo@outlook.com>
2026-02-24 20:43:05 -0300

    Exemplos de internacionalizacao de Datas, Números e moedas

--- DevDojo/javacore/Sformatacao/test/DateFormatTest01.java
+++ DevDojo/javacore/Sformatacao/test/DateFormatTest01.java
@@ -1,4 +1,4 @@
-package DevDojo.javacore.Rdatas.test;
+package DevDojo.javacore.Sformatacao.test;
 
 import java.text.DateFormat;
 import java.util.Calendar;
---
+++ DevDojo/javacore/Sformatacao/test/LocaleTest01.java
@@ -0,0 +1,33 @@
+package DevDojo.javacore.Sformatacao.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocaleTest01 {
+    public static void main(String[] args) {
+        Locale LocaleItaly = new Locale("it", "IT");
+        Locale LocaleCH = new Locale("it", "CH");
+        Locale LocaleIndia = new Locale("hi", "IN");
+        Locale LocaleJapao = new Locale("ja", "JP");
+        Locale LocaleHolanda = new Locale("nl", "NL");
+
+        Calendar calendar = Calendar.getInstance();
+
+        DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, LocaleItaly);
+        DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, LocaleCH);
+        DateFormat df3 = DateFormat.getDateInstance(DateFormat.FULL, LocaleIndia);
+        DateFormat df4 = DateFormat.getDateInstance(DateFormat.FULL, LocaleJapao);
+        DateFormat df5 = DateFormat.getDateInstance(DateFormat.FULL, LocaleHolanda);
+
+        System.out.println("Italia "+df1.format(calendar.getTime()));
+        System.out.println("Suíça "+ df2.format(calendar.getTime()));
+        System.out.println("India "+ df3.format(calendar.getTime()));
+        System.out.println("Japão "+ df4.format(calendar.getTime()));
+        System.out.println("Holanda "+ df5.format(calendar.getTime()));
+
+        System.out.println(LocaleItaly.getDisplayCountry());
+        System.out.println(LocaleCH.getDisplayCountry(LocaleIndia));
+        System.out.println(LocaleItaly.getDisplayLanguage(LocaleJapao));
+    }
+}
