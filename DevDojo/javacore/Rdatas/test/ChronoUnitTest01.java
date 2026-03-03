e5a8a54052070855f13d80b780097b53fb2e3d0b
Junionuno <junio.geraldo@outlook.com>
2026-03-02 22:06:36 -0300

    Mais exemplos de Classes utilitarias, Instant, Duration, Period, ChronoUnit e TemporalAdjusters

---
+++ DevDojo/javacore/Rdatas/test/ChronoUnitTest01.java
@@ -0,0 +1,19 @@
+package DevDojo.javacore.Rdatas.test;
+
+import Exercícios.curso.dominio.Local;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+    public static void main(String[] args) {
+        LocalDateTime aniversario = LocalDateTime.of(2004, Month.AUGUST, 15, 12, 0, 0);
+        LocalDateTime now = LocalDateTime.now();
+
+        System.out.println(ChronoUnit.DAYS.between(aniversario, now));
+        System.out.println(ChronoUnit.WEEKS.between(aniversario, now));
+        System.out.println(ChronoUnit.MONTHS.between(aniversario, now));
+        System.out.println(ChronoUnit.YEARS.between(aniversario, now));
+    }
+}
