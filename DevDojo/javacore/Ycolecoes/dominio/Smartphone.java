49450a95f617d9ad56a59479635f698238930e99
Junionuno <junio.geraldo@outlook.com>
2026-05-18 21:18:19 -0300

    Exemplo sobre Hashcode

--- DevDojo/javacore/Ycolecoes/dominio/Smartphone.java
+++ DevDojo/javacore/Ycolecoes/dominio/Smartphone.java
@@ -22,6 +22,15 @@ public class Smartphone {
         return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
     }
 
+    // se x.equals(y) == true, y.hashcode == x.hashCode()
+    // y.hashcode() == s.hashcorde() não necessariamente o equals de y.equals(x) tem que ser true
+    // x.equals(y) == false
+    // y.hashcode() != x.hashcode() x.equals(y) deverá ser false.
+    @Override
+    public int hashCode() {
+        return serialNumber == null ? 0 : this.serialNumber.hashCode();
+    }
+
     public void setSerialNumber(String serialNumber) {
         this.serialNumber = serialNumber;
     }
